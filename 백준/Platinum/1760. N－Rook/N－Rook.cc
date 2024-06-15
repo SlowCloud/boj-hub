646048310161503b76336d6c351f3889f36342fc
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2024-06-15 16:16:43 +0900

    [Platinum III] Title: N-Rook, Time: 8 ms, Memory: 3800 KB -BaekjoonHub

---
+++ 백준/Platinum/1760. N－Rook/N－Rook.cc
@@ -0,0 +1,113 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <string.h>
+#include <cstring>
+#include <algorithm>
+#include <math.h>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <unordered_set>
+#include <stack>
+#include <unordered_map>
+#include <map>
+#include <numeric>
+using namespace std;
+
+using ll = long long;
+using ii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define FOr(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define endl '\n'
+
+
+int R, C, N;
+
+int board[333][333];
+
+int l[333][333], r[333][333];
+
+bool out(int x, int y) {
+	return min(x, y) < 0 || max(x, y) >= N;
+}
+
+int LL, RR;
+
+void buildl() {
+	LL = 0;
+	For(i, 0, R) {
+		For(j, 0, C) {
+			if (board[i][j] == 2) {
+				LL++; continue;
+			}
+			l[i][j] = LL;
+		}
+		LL++;
+	}
+}
+
+void buildr() {
+	RR = 0;
+	For(j, 0, C) {
+		For(i, 0, R) {
+			if (board[i][j] == 2) {
+				RR++; continue;
+			}
+			r[i][j] = RR;
+		}
+		RR++;
+	}
+}
+
+vector<int> G[11'111];
+
+int vst[11'111];
+int p[11'111];
+
+int dfs(int now) {
+	if (vst[now]) return false;
+	vst[now] = 1;
+	for (int next : G[now]) {
+		if (p[next] == -1 || dfs(p[next])) {
+			p[next] = now; return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	cin >> R >> C;
+
+	For(i, 0, R) {
+		For(j, 0, C) {
+			cin >> board[i][j];
+		}
+	}
+
+	buildl();
+	buildr();
+
+	For(i, 0, R) For(j, 0, C) {
+		if (board[i][j]) continue;
+		G[l[i][j]].push_back(r[i][j]);
+	}
+
+	memset(p, -1, sizeof p);
+
+	int res = 0;
+	For(i, 0, LL) {
+		memset(vst, 0, sizeof vst);
+		res += dfs(i);
+	}
+	
+	cout << res;
+
+}
