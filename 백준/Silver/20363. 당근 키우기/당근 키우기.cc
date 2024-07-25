3656f9b06efc8941e3ab9f2de9d4e863b7243e88
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2024-07-25 21:02:56 +0900

    [Silver IV] Title: 당근 키우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/20363. 당근 키우기/당근 키우기.cc
@@ -0,0 +1,32 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int X, Y; cin >> X >> Y;
+
+	int a, b; a = b = 0;
+
+	a = X + (Y / 10) + Y;
+	b = Y + (X / 10) + X;
+
+	cout << min(a, b);
+
+}
