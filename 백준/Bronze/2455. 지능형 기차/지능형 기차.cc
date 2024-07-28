cba68d40a8be9ca8b82c7c0ee58d20a3f1fc567f
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2024-07-28 22:20:28 +0900

    [Bronze III] Title: 지능형 기차, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2455. 지능형 기차/지능형 기차.cc
@@ -0,0 +1,35 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int now = 0;
+	int res = 0;
+
+
+	For(i, 0, 4) {
+		int a, b; cin >> a >> b;
+		now = now - a + b;
+		res = max(res, now);
+	}
+
+	cout << res;
+
+}
