3dcae2b492db97c26d647ca41b5a59d1537ba056
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-02-05 11:07:18 +0900

    [Bronze III] Title: 연세여 사랑한다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25915. 연세여 사랑한다/연세여 사랑한다.cc
@@ -0,0 +1,36 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	const string s = "ILOVEYONSEI";
+
+	char c; cin >> c;
+
+	int res = 0;
+	for (char cc : s) {
+		res += abs(c - cc);
+		c = cc;
+	}
+
+	cout << res;
+
+}
