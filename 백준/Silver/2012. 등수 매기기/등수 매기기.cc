fe19259344641ab33afabc1b3a75560668324152
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-03-30 18:42:11 +0900

    [Silver III] Title: 등수 매기기, Time: 76 ms, Memory: 3980 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.cc
@@ -0,0 +1,40 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int N; cin >> N;
+
+	vector<int> v(N);
+	for (auto& i : v) cin >> i;
+
+	sort(all(v));
+
+	ll res = 0;
+	FOR(i, 0, N) {
+		res += abs(i + 1 - v[i]);
+	}
+
+	cout << res;
+
+}
