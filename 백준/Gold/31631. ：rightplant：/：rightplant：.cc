e979b121e536eeec04f50c2a71490a002a340d03
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-03-31 23:12:20 +0900

    [Gold III] Title: :rightplant:, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/31631. ：rightplant：/：rightplant：.cc
+++ 백준/Gold/31631. ：rightplant：/：rightplant：.cc
@@ -20,65 +20,6 @@ using ii = pair<int, int>;
 
 
 
-int calc(const vector<int>& v) {
-
-	int res = 0;
-	FOR(i, 0, v.size()) {
-		vector<int> cp = v;
-
-		int d = 1;
-		int score = 0;
-		int idx = i;
-		int height = v[i];
-
-		while (1) {
-			idx += d;
-			if (idx < 0 || idx >= v.size()) break;
-			if (cp[idx] > height) {
-				cp[idx]--; score++; d *= -1;
-			}
-		}
-
-		res += score;
-
-	}
-
-	return res;
-}
-
-void test(int N) {
-	vector<int> v(N);
-	iota(all(v), 1);
-
-	int score = 0;
-	vector<vector<int>> arrs;
-	do {
-		int now = calc(v);
-		if (now > score) {
-			score = now;
-			arrs = { v };
-		}
-		else if (now == score) {
-			arrs.push_back(v);
-		}
-	} while (next_permutation(all(v)));
-
-	cout << score << endl;
-	for (auto& arr : arrs) {
-		for (auto& i : arr) cout << i << ' '; cout << endl;
-	}
-	cout << "------" << endl;
-
-}
-
-/*
-
-N이 홀수라면 왼쪽으로 2번 오른쪽으로 2번 넣으면 된다.
-N이 짝수라면 1을 넣고 진행하면 된다.
-
-큰 수가 오른쪽으로 나오도록 출력하면 끝.
-
-*/
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
@@ -86,34 +27,27 @@ int main() {
 
 	int N; cin >> N;
 
-	deque<int> dq;
+	vector<int> v(N, 1);
 
-	int d = 1;
-	int cnt = 2;
+	int l, r, n, flip;
+	l = n = flip = 0;
+	r = N - 1;
+	n = N;
 
-	if (N % 2 == 0) {
-		dq.push_back(1);
-	}
-
-	FOR(i, 1 + !(N % 2), N + 1) {
-		if (d) dq.push_front(i);
-		else dq.push_back(i);
-		cnt--;
-		if (cnt == 0) {
-			cnt = 2; d ^= 1;
+	while (l < r) {
+		if (flip) {
+			v[l] = n; v[r] = n - 1;
 		}
+		else {
+			v[r] = n; v[l] = n - 1;
+		}
+		flip ^= 1;
+		l++; r--;
+		n -= 2;
 	}
 
-	if (dq.front() < dq.back()) {
-		while (dq.size()) {
-			cout << dq.front() << ' '; dq.pop_front();
-		}
-	}
-	else {
-		while (dq.size()) {
-			cout << dq.back() << ' '; dq.pop_back();
-		}
-	}
+	for (auto& i : v) cout << i << ' ';
+
 
 }
 
