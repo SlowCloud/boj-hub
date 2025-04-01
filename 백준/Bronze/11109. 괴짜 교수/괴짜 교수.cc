7eef5549defd83662d515459a71a9e644046fde8
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-04-01 09:08:25 +0900

    [Bronze III] Title: 괴짜 교수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11109. 괴짜 교수/괴짜 교수.cc
@@ -0,0 +1,46 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int T; cin >> T;
+	while (T--) {
+		ll d, n, s, p; cin >> d >> n >> s >> p;
+
+		ll a = s * n;
+		ll b = d + p * n;
+
+		if (a < b) {
+			cout << "do not parallelize" << endl;
+		}
+		else if (a > b) {
+			cout << "parallelize" << endl;
+		}
+		else {
+			cout << "does not matter" << endl;
+		}
+
+	}
+
+}
+
