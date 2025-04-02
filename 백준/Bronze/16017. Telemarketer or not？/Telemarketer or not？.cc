12586b742b1da9329e60059977ff8beff4ff9e1c
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-04-02 21:21:55 +0900

    [Bronze IV] Title: Telemarketer or not?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16017. Telemarketer or not？/Telemarketer or not？.cc
@@ -0,0 +1,42 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	vector<int> s(4);
+	for (auto& i : s) cin >> i;
+
+	if (s[0] != 8 && s[0] != 9) {
+		cout << "answer"; return 0;
+	}
+	if (s[3] != 8 && s[3] != 9) {
+		cout << "answer"; return 0;
+	}
+	if (s[1] != s[2]) {
+		cout << "answer"; return 0;
+	}
+	cout << "ignore";
+
+
+}
+
