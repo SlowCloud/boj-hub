27b2f31dea18b9f9102fd98df1a8e0f4384cdd25
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-04-07 17:49:42 +0900

    [Gold II] Title: 최대 거리, Time: 16 ms, Memory: 2792 KB -BaekjoonHub

---
+++ 백준/Gold/2381. 최대 거리/최대 거리.cc
@@ -0,0 +1,41 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int N; cin >> N;
+
+	vector<int> a, b;
+
+	FOR(i, 0, N) {
+		int u, v; cin >> u >> v;
+		a.push_back(u + v);
+		b.push_back(u - v);
+	}
+
+	sort(all(a));
+	sort(all(b));
+
+	cout << max(a.back() - a.front(), b.back() - b.front());
+
+}
