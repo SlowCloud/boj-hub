18e137c5c51168ebc201e2a8a96edb246bdee5e9
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-04-08 18:53:21 +0900

    [Bronze IV] Title: Triathlon, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25600. Triathlon/Triathlon.cc
@@ -0,0 +1,43 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int N; cin >> N;
+
+	ll res = 0;
+
+	FOR(i, 0, N) {
+		ll a, b, c; cin >> a >> b >> c;
+
+		ll now = a * (b + c);
+		if (a == (b + c)) {
+			now <<= 1;
+		}
+
+		res = max(res, now);
+	}
+
+	cout << res;
+
+}
