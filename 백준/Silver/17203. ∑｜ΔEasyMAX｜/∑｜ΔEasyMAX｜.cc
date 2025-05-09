94a147c66b1a462b119ff0099cb23e571d25f87c
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-05-10 08:44:08 +0900

    [Silver IV] Title: ∑|ΔEasyMAX|, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Silver/17203. ∑｜ΔEasyMAX｜/∑｜ΔEasyMAX｜.cc
@@ -0,0 +1,44 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int N, Q; cin >> N >> Q;
+
+	vector<int> v(N);
+	for (auto& i : v) cin >> i;
+
+	vector<int> res(2);
+	FOR(i, 1, v.size()) {
+		res.push_back(abs(v[i] - v[i - 1]));
+	}
+	FOR(i, 1, res.size()) {
+		res[i] += res[i - 1];
+	}
+
+	FOR(i, 0, Q) {
+		int a, b; cin >> a >> b;
+		cout << res[b] - res[a] << endl;
+	}
+
+}
+
