f4f16c58f965486593b4ad0ff63c2612626e3bc3
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-06-06 16:48:30 +0900

    [Gold V] Title: 피타고라스 정리의 증명, Time: 144 ms, Memory: 2020 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Gold/33466. 피타고라스 정리의 증명/피타고라스 정리의 증명.cc
@@ -0,0 +1,36 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int T; cin >> T;
+	while (T--) {
+		int N; cin >> N;
+
+		int res = 0;
+		res += N; // (1,1), (2,2), ... (N,N)
+		res += N/2*2; // {(1,2), (2,4), ... (N/2, N/2*2)} * 2
+
+		cout << res << endl;
+	}
+
+}
