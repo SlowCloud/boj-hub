fc3e269bdc96e50c86dbfc907ce6b45db6498c2c
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-06-15 21:49:51 +0900

    [Silver I] Title: 너 그리고 나 (NAVILLERA), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/32282. 너 그리고 나 （NAVILLERA）/너 그리고 나 （NAVILLERA）.cc
@@ -0,0 +1,43 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int X, Y, C; cin >> X >> Y >> C;
+
+	if (X == 0 && Y == 0) {
+		cout << 0; exit(0);
+	}
+
+	int dist = X * X + Y * Y;
+
+	int CC = C * C;
+
+	int gap = (int)sqrt(dist / CC);
+
+	if (gap * gap != dist/CC || dist % CC) {
+		gap += gap ? 1 : 2;
+	}
+
+	cout << gap;
+
+}
