cdbb019b47a155321235ff58b20e2ca050e9b325
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-06-18 22:59:43 +0900

    [Bronze III] Title: 3n+1 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14920. 3n＋1 수열/3n＋1 수열.cc
@@ -0,0 +1,36 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	ll N; cin >> N;
+
+	int res = 1;
+	while (N != 1) {
+		res++;
+		N = N & 1 ? N * 3 + 1 : N / 2;
+	}
+
+	cout << res;
+
+}
