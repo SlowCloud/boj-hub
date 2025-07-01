8b7913107bbc70d0dedc269a2846bb6c8707162b
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-07-01 13:48:34 +0900

    [Gold III] Title: 여왕벌, Time: 240 ms, Memory: 4380 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/10836. 여왕벌/여왕벌.cc
+++ 백준/Gold/10836. 여왕벌/여왕벌.cc
@@ -21,71 +21,52 @@ using ii = pair<int, int>;
 
 int board[777][777];
 
-void prepare_board(const int M) {
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int M, N; cin >> M >> N;
+
+	vector<int> v(M * 2 - 1);
+	v[0] = 1;
+
+	FOR(i, 0, N) {
+		int a, b, c; cin >> a >> b >> c;
+		int idx = 0;
+
+		idx += a;
+		if (idx >= v.size()) continue;
+		v[idx] += 1;
+
+		idx += b;
+		if (idx >= v.size()) continue;
+		v[idx] += 1;
+	}
+
+	FOR(i, 1, v.size()) {
+		v[i] += v[i - 1];
+	}
+
+	// for (auto& i : v) cout << i << ' ';
+
 	FOR(i, 0, M) {
-		board[i][0] = board[0][i] = 1;
-	}
-}
-
-vector<int> build_grow(const vector<int>& v) {
-	vector<int> res;
-	FOR(i, 0, v[0]) {
-		res.push_back(0);
-	}
-	FOR(i, 0, v[1]) {
-		res.push_back(1);
-	}
-	FOR(i, 0, v[2]) {
-		res.push_back(2);
-	}
-	return res;
-}
-
-void build_board(const int M, const vector<int>& total) {
-
-	FOR(i, 0, M) {
-		board[M - i - 1][0] += total[i];
+		board[M - i - 1][0] = v[i];
 	}
 
-	FOR(i, M, M * 2 - 1) {
-		board[0][i - M + 1] += total[i];
+	FOR(i, M, v.size()) {
+		board[0][i - M + 1] = v[i];
 	}
 
 	FOR(i, 1, M) {
 		FOR(j, 1, M) {
-			int now = max({ board[i - 1][j], board[i][j - 1], board[i - 1][j - 1] });
-			board[i][j] = now;
+			board[i][j] = max(board[i - 1][j], board[i][j - 1]);
 		}
 	}
-}
 
-void print_board(const int M) {
 	FOR(i, 0, M) {
 		FOR(j, 0, M) {
 			cout << board[i][j] << ' ';
 		}
 		cout << endl;
 	}
-}
-
-int main() {
-	cin.tie(0)->sync_with_stdio(0);
-
-	int M, N; cin >> M >> N;
-
-	prepare_board(M);
-
-	vector<int> total(M * 2 - 1);
-	FOR(i, 0, N) {
-		vector<int> v(3);
-		for (auto& i : v) cin >> i;
-		vector<int> w = build_grow(v);
-		FOR(i, 0, w.size()) {
-			total[i] += w[i];
-		}
-	}
-
-	build_board(M, total);
-	print_board(M);
 
 }
