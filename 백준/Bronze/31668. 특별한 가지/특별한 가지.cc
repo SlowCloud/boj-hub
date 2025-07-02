3b6323a468ea2ddf736a09c0c1acb588e14e2ba4
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-07-02 12:51:20 +0900

    [Bronze IV] Title: 특별한 가지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31668. 특별한 가지/특별한 가지.cc
@@ -0,0 +1,32 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+
+// https://www.acmicpc.net/problem/5911
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int N, M, K; cin >> N >> M >> K;
+
+	cout << M / N * K;
+
+}
