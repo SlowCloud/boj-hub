0ff2ef5bb2e676c425eee527591552aef5255c6b
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-07-07 11:59:23 +0900

    [Silver II] Title: 내 집 마련하기, Time: 8 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/30619. 내 집 마련하기/내 집 마련하기.cc
@@ -0,0 +1,61 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+
+int idx[333];
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int N; cin >> N;
+
+
+	vector<int> v(N);
+	FOR(i, 0, N) {
+		int n; cin >> n;
+		v[i] = n;
+		idx[n] = i;
+	}
+
+	int Q; cin >> Q;
+	while (Q--) {
+		int a, b; cin >> a >> b;
+
+		vector<int> w;
+		FOR(i, a, b + 1) {
+			w.push_back(idx[i]);
+		}
+
+		sort(all(w));
+		vector<int> vv = v;
+		int now = a;
+		for (auto& i : w) {
+			vv[i] = now;
+			now++;
+		}
+
+		for (auto& i : vv) cout << i << ' ';
+		cout << endl;
+
+	}
+
+}
+
