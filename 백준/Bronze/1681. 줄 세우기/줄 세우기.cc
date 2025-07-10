7f7cd54a28f9548d1e09fd7eb8cf390314991740
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-07-10 19:27:18 +0900

    [Bronze II] Title: 줄 세우기, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1681. 줄 세우기/줄 세우기.cc
@@ -0,0 +1,50 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+
+bool check(ll now, const int L) {
+	while (now) {
+		int n = now % 10; now /= 10;
+		if (n == L) return true;
+	}
+	return false;
+}
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+
+	int N; cin >> N;
+	int L; cin >> L;
+
+	ll now = 0;
+	while(1) {
+		now++;
+		if (check(now, L)) {
+			continue;
+		}
+		N--;
+		if (N == 0) break;
+	}
+
+	cout << now;
+
+}
