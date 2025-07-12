7d6fb942ad5b64b05fb2f16e00e24d4c061f08da
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-07-12 21:09:58 +0900

    [Silver II] Title: 한 줄로 서기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.cc
@@ -0,0 +1,40 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int N; cin >> N;
+	vector<int> v(N);
+	FOR(i,0,N) {
+		int n; cin >> n;
+		int idx = 0;
+		while (1) {
+			while (v[idx]) idx++;
+			if (n == 0) break;
+			n--; idx++;
+		}
+		v[idx] = i + 1;
+	}
+	for (auto& i : v) cout << i << ' ';
+
+}
