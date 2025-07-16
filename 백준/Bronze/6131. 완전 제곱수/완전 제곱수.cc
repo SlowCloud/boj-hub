c4fa5a5972a3f88d1244ee5f2bd655d381ca842a
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-07-16 13:32:26 +0900

    [Bronze III] Title: 완전 제곱수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.cc
@@ -0,0 +1,33 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int N; cin >> N;
+
+	int res = 0;
+	FOR(i, 1, 501) FOR(j, 1, 501) {
+		res += i * i - j * j == N;
+	}
+	cout << res;
+
+}
