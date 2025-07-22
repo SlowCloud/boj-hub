9bed8462be984507e157cbe4040ff20b8e7a8838
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-07-23 08:09:35 +0900

    [Bronze I] Title: 성 지키기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/1236. 성 지키기/성 지키기.cc
@@ -0,0 +1,44 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+using ii = pair<int, int>;
+
+#define FOR(i,a,b) For(i,a,b)
+#define For(i,a,b) for(int i=a;i<b;i++)
+#define foi(i,n) for(int i=0;i<n;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int R, C;
+	cin >> R >> C;
+
+	set<int> a, b;
+	FOR(i, 0, R) {
+		FOR(j, 0, C) {
+			char c; cin >> c;
+			if (c == 'X') {
+				a.insert(i);
+				b.insert(j);
+			}
+		}
+	}
+
+	int l = R - a.size();
+	int r = C - b.size();
+	cout << max(l, r);
+
+}
