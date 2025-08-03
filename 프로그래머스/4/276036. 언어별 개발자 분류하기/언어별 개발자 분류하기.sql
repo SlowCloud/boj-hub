4de22e1df6dec0ba77cad96b9841d2ae9ddd668b
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-08-03 11:11:09 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -0,0 +1,27 @@
+-- 코드를 작성해주세요
+
+select * from (
+
+with info as (
+    select *
+    from skillcodes a
+    left join developers b
+    on a.code & b.skill_code
+)
+select (
+    case
+    when
+        (id, 'Front end') in (select id, category from info) and
+        (id, 'Python') in (select id, name from info)
+    then 'A'
+    when (id, 'C#') in (select id, name from info)
+    then 'B'
+    when (id, 'Front end') in (select id, category from info)
+    then 'C'
+    end
+) as grade, id, email
+from developers
+order by grade, id asc
+    
+) result
+where grade is not null
