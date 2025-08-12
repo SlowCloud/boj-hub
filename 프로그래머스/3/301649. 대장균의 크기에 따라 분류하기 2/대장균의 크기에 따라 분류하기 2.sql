86efdf389b9654524e21608f6daa81eb5790bf5b
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-08-12 13:53:51 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -0,0 +1,10 @@
+-- 코드를 작성해주세요
+
+select id, case
+    when percent_rank() over (order by size_of_colony desc) <= 0.25 then 'CRITICAL'
+    when percent_rank() over (order by size_of_colony desc) <= 0.5 then 'HIGH'
+    when percent_rank() over (order by size_of_colony desc) <= 0.75 then 'MEDIUM'
+    else 'LOW'
+    end colony_name
+from ecoli_data
+order by id asc
