d1516e59d67ce6d46934d90bd285717dbd0ff0c3
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-09-05 14:02:43 +0900

    [Silver II] Title: 초콜릿 뺏어 먹기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/23322. 초콜릿 뺏어 먹기/초콜릿 뺏어 먹기.cc
@@ -0,0 +1,41 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+#include <regex>
+using namespace std;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+ 
+	int N, K; cin >> N >> K;
+
+	vector<int> v(N);
+	for (auto& i : v) cin >> i;
+	sort(all(v));
+
+	int small = v[0];
+	for (auto& i : v) i -= small;
+
+	int cnt = 0;
+	int res = 0;
+	for (const auto& i : v) {
+		cnt += i != 0;
+		res += i;
+	}
+
+	cout << res << ' ' << cnt;
+
+}
