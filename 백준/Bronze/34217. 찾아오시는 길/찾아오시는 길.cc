fbdb298d12e74a97afdca2bb9459b284ecf06d8d
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-09-10 13:28:40 +0900

    [Bronze V] Title: 찾아오시는 길, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34217. 찾아오시는 길/찾아오시는 길.cc
@@ -0,0 +1,28 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+#include <regex>
+using namespace std;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int a, b, c, d; cin >> a >> b >> c >> d;
+
+	if (a + c < b + d) cout << "Hanyang Univ.";
+	else if (a + c > b + d) cout << "Yongdap";
+	else cout << "Either";
+
+}
