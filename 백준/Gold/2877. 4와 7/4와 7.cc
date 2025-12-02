cf167e0395258aa41f6054924d631f153a054c63
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-12-02 12:43:29 +0900

    [Gold V] Title: 4와 7, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/2877. 4와 7/4와 7.cc
+++ 백준/Gold/2877. 4와 7/4와 7.cc
@@ -1,3 +1,3 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){int N,i;cin>>N;N+=1;for(i=log2(N)-1;i>=0;i--){cout<<(N&(1<<i)?7:4);}}
+int main(){int N,i;cin>>N;N++;for(i=log2(N)-1;i>=0;i--){cout<<(N&(1<<i)?7:4);}}
