11fd7d5ae3c0287724a54e1be451309733a73489
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2025-12-05 18:02:48 +0900

    [Gold IV] Title: 수들의 합 4, Time: 40 ms, Memory: 13928 KB -BaekjoonHub

---
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.cc
@@ -0,0 +1,43 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+#include <regex>
+using namespace std;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+
+#define FOR(i,a,b) for(int i=a;i<b;i++)
+#define endl '\n'
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+
+
+ll dp[222'222];
+
+map<ll, vector<int>> pos;
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int N, K; cin >> N >> K;
+
+	FOR(i, 1, N + 1) cin >> dp[i];
+	FOR(i, 2, N + 1) dp[i] += dp[i - 1];
+
+	ll res = 0;
+	FOR(i, 1, N + 1) {
+		const auto& now = dp[i];
+		if (pos.find(now - K) != pos.end()) {
+			res += pos[now - K].size();
+		}
+		if (now == K) res++;
+		pos[now].push_back(i);
+	}
+
+	cout << res;
+
+}
