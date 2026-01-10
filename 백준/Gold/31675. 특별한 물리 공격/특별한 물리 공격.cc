77d04bcf3c94d06caeacd85868076f387049bdf3
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2026-01-10 13:05:36 +0900

    [Gold III] Title: 특별한 물리 공격, Time: 12 ms, Memory: 4540 KB -BaekjoonHub

---
+++ 백준/Gold/31675. 특별한 물리 공격/특별한 물리 공격.cc
@@ -0,0 +1,178 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+#include <regex>
+#include <algorithm>
+using namespace std;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+
+#define FOR_(type,i, a, b) for(type i=a;i<b;i++)
+#define FOR(i,a,b) FOR_(int,i,a,b)
+#define FORLL(i,a,b) FOR_(ll,i,a,b)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define endl '\n'
+
+
+struct Edge {
+	int to;
+	ll cost;
+	ll cap;
+	int rev;
+
+	Edge(int to, ll cost): to(to), cost(cost), rev(0), cap(0) {}
+	Edge(int to, ll cost, ll cap, int rev): to(to), cost(cost), cap(cap), rev(rev) {}
+};
+
+class Graph {
+public:
+	int n;
+	vector<vector<Edge>> adj;
+	Graph(int n) : n(n), adj(n) {}
+
+	void add_edge(int u, int v) {
+		add_edge(u, v, 1ll);
+	}
+
+	void add_edge(int u, int v, ll cost) {
+		adj[u].push_back(Edge{ v,cost });
+	}
+
+	vector<ll> dijkstra(const int s) const {
+		vector<ll> d(n, -1);
+		priority_queue<pll, vector<pll>, greater<>> pq;
+		pq.push({ 0,s });
+		d[s] = 0;
+		while (pq.size()) {
+			auto [_, now] = pq.top(); pq.pop();
+			for (const auto& next : adj[now]) {
+				if (d[next.to] == -1 || d[next.to] > d[now] + next.cost) {
+					d[next.to] = d[now] + next.cost;
+					pq.push({ d[next.to], next.to });
+				}
+			}
+		}
+		return d;
+	}
+
+};
+
+class MCMF : public Graph {
+private:
+
+	vector<int> p;
+	vector<ll> d;
+	vector<int> Q;
+	vector<int> idx;
+
+
+	void spfa(const int s, const int e) {
+		fill(all(Q), 0);
+		fill(all(d), INT_MAX);
+		fill(all(p), -1);
+		queue<int> q;
+		q.push(s);
+		d[s] = 0;
+		while (q.size()) {
+			int now = q.front(); q.pop();
+			Q[now] = 0;
+			FOR(i, 0, adj[now].size()) {
+				const auto& next = adj[now][i];
+				if (next.cap <= 0) continue;
+				if (d[next.to] > d[now] + next.cost) {
+					d[next.to] = d[now] + next.cost;
+					p[next.to] = now;
+					idx[next.to] = i;
+					if (!Q[next.to]) {
+						Q[next.to] = 1;
+						q.push(next.to);
+					}
+				}
+			}
+		}
+	}
+	ll flow_once(const int s, const int e) {
+		ll f = LLONG_MAX;
+		for (int now = e; now != s; now = p[now]) {
+			f = min(f, adj[p[now]][idx[now]].cap);
+		}
+		for (int now = e; now != s; now = p[now]) {
+			adj[p[now]][idx[now]].cap -= f;
+			adj[now][adj[p[now]][idx[now]].rev].cap += f;
+		}
+		return f;
+	}
+
+public:
+	MCMF(int n) : Graph(n), p(n), d(n), Q(n), idx(n) {}
+
+	void add_edge(int u, int v, ll cost, ll cap) {
+		adj[u].push_back(Edge{ v,cost,cap,(int)adj[v].size() });
+		adj[v].push_back(Edge{ u,-cost,0,(int)adj[u].size() - 1 });
+	}
+
+	pair<ll, ll> total_flow(const int s, const int e) {
+		ll f = 0;
+		ll c = 0;
+		while(1) {
+			spfa(s, e);
+			if (p[e] == -1) break;
+			ll now = flow_once(s, e);
+			f += now;
+			c += d[e] * now;
+		}
+		return { f, c };
+	}
+};
+
+
+
+
+ll dp[111'111][2];
+
+// dp[i][1] = dp[i-1][0] + v[i]
+// dp[i][0] = max(dp[i-2][0], dp[i-2][1]) + v[i]
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	int N; cin >> N;
+
+	// edges
+	if (N == 1) {
+		cout << 0; exit(0);
+	}
+	if (N == 2) {
+		int a, b; cin >> a >> b;
+		cout << max(a, b); exit(0);
+	}
+
+	vector<ll> v(N);
+	for (auto& x : v) cin >> x;
+
+	FOR(i, 0, min(N, 2)) {
+		dp[i][0] = v[i];
+		//if (i) dp[i][1] = dp[i - 1][0] + v[i]; // 첫 번째 녀석은 연달아 지우면 안됨..
+	}
+
+	FOR(i, 2, N) {
+		dp[i][1] = dp[i - 1][0] + v[i];
+		dp[i][0] = max(dp[i - 2][0], dp[i - 2][1]) + v[i];
+	}
+
+	ll res = 0;
+	FOR(i, N - 2, N) {
+		FOR(j, 0, 2) {
+			if (i == N - 1 && j == 1) continue; // 역시 마지막도 연달아 지우면 안됨
+			res = max(res, dp[i][j]);
+		}
+	}
+
+	cout << res;
+
+}
