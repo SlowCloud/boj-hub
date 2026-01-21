fa30d5775ea69e6e43ced4451a9dcda3f6604e8d
SlowCloud <79568594+SlowCloud@users.noreply.github.com>
2026-01-21 12:09:27 +0900

    [Gold V] Title: 무한 수열 2, Time: 248 ms, Memory: 45424 KB -BaekjoonHub

---
+++ 백준/Gold/1354. 무한 수열 2/무한 수열 2.cc
@@ -0,0 +1,162 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+#include <regex>
+#include <algorithm>
+using namespace std;
+
+using ll = long long;
+using pll = pair<ll, ll>;
+using pii = pair<int, int>;
+
+#define FOR(i,a,b) for(auto i=(a);i<(b);i++)
+#define all(v) v.begin(), v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define endl '\n'
+
+#define OUT(R,C) [](const auto x, const auto y) -> bool { return (x >= 0 && x < (R) && y >= 0 && y < (C)); }
+#define POWLL(mod) [](ll n, ll k) { ll r = 1 % mod; while (k) { if (k & 1) r = (r * n) % mod; n = (n * n) % mod; k >>= 1; } return r; }
+
+struct Edge {
+	int to;
+	ll cost;
+	ll cap;
+	int rev;
+
+	Edge(int to, ll cost): to(to), cost(cost), rev(0), cap(0) {}
+	Edge(int to, ll cost, ll cap, int rev): to(to), cost(cost), cap(cap), rev(rev) {}
+};
+
+class Graph {
+public:
+	int n;
+	vector<vector<Edge>> adj;
+	Graph(int n) : n(n), adj(n) {}
+
+	void add_edge(int u, int v) {
+		add_edge(u, v, 1ll);
+	}
+
+	void add_edge(int u, int v, ll cost) {
+		adj[u].push_back(Edge{ v,cost });
+	}
+
+	vector<ll> dijkstra(const int s) const {
+		vector<ll> d(n, -1);
+		priority_queue<pll, vector<pll>, greater<>> pq;
+		pq.push({ 0,s });
+		d[s] = 0;
+		while (pq.size()) {
+			auto [_, now] = pq.top(); pq.pop();
+			for (const auto& next : adj[now]) {
+				if (d[next.to] == -1 || d[next.to] > d[now] + next.cost) {
+					d[next.to] = d[now] + next.cost;
+					pq.push({ d[next.to], next.to });
+				}
+			}
+		}
+		return d;
+	}
+
+};
+
+class MCMF : public Graph {
+private:
+
+	vector<int> p;
+	vector<ll> d;
+	vector<int> Q;
+	vector<int> idx;
+
+
+	void spfa(const int s, const int e) {
+		fill(all(Q), 0);
+		fill(all(d), INT_MAX);
+		fill(all(p), -1);
+		queue<int> q;
+		q.push(s);
+		d[s] = 0;
+		while (q.size()) {
+			int now = q.front(); q.pop();
+			Q[now] = 0;
+			FOR(i, 0, adj[now].size()) {
+				const auto& next = adj[now][i];
+				if (next.cap <= 0) continue;
+				if (d[next.to] > d[now] + next.cost) {
+					d[next.to] = d[now] + next.cost;
+					p[next.to] = now;
+					idx[next.to] = i;
+					if (!Q[next.to]) {
+						Q[next.to] = 1;
+						q.push(next.to);
+					}
+				}
+			}
+		}
+	}
+	ll flow_once(const int s, const int e) {
+		ll f = LLONG_MAX;
+		for (int now = e; now != s; now = p[now]) {
+			f = min(f, adj[p[now]][idx[now]].cap);
+		}
+		for (int now = e; now != s; now = p[now]) {
+			adj[p[now]][idx[now]].cap -= f;
+			adj[now][adj[p[now]][idx[now]].rev].cap += f;
+		}
+		return f;
+	}
+
+public:
+	MCMF(int n) : Graph(n), p(n), d(n), Q(n), idx(n) {}
+
+	void add_edge(int u, int v, ll cost, ll cap) {
+		adj[u].push_back(Edge{ v,cost,cap,(int)adj[v].size() });
+		adj[v].push_back(Edge{ u,-cost,0,(int)adj[u].size() - 1 });
+	}
+
+	pair<ll, ll> total_flow(const int s, const int e) {
+		ll f = 0;
+		ll c = 0;
+		while(1) {
+			spfa(s, e);
+			if (p[e] == -1) break;
+			ll now = flow_once(s, e);
+			f += now;
+			c += d[e] * now;
+		}
+		return { f, c };
+	}
+};
+
+
+
+ll N, P, Q, X, Y;
+constexpr int SIZE = 5'555'555;
+ll m[SIZE];
+
+ll A(ll i) {
+	if (i <= 0) return 1;
+	if (i < SIZE) {
+		return m[i];
+	}
+	return A(i / P - X) + A(i / Q - Y);
+}
+
+
+int main() {
+	cin.tie(0)->sync_with_stdio(0);
+
+	cin >> N >> P >> Q >> X >> Y;
+
+	FOR(i, 1, SIZE) {
+		int l = i / P - X;
+		int r = i / Q - Y;
+		m[i] = (l <= 0 ? 1 : m[l]) + (r <= 0 ? 1 : m[r]);
+	}
+
+	cout << A(N);
+
+
+}
